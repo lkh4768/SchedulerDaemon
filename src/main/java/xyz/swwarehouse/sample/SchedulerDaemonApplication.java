e8665ef3d648e9a68add6d781d7e6d61dfe86d5a
wes.lee <lkh5510@gmail.com>
2017-11-21 16:17:58 +0900

    first commit

---
+++ src/main/java/xyz/swwarehouse/sample/SchedulerDaemonApplication.java
@@ -0,0 +1,12 @@
+package xyz.swwarehouse.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerDaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerDaemonApplication.class, args);
+	}
+}
